884a2bbef39d548b0bb2cc8b845f39446e72070c
zerodayz77 <charisyt26@gmail.com>
2022-06-24 20:44:07 -0400

    fist push with basic layout so far

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <h1>My Portfolio</h1>
+      <p>This will be used to showcase all of my skills and projects</p>
+    </div>
+  );
+}
